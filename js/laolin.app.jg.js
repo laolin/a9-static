725b21af83cff36270e43fbb3e3502734f2a0e48
LaoLin <hi@laolin.com>
2013-08-08 03:07:01 +0800

    prepare for laolin.app.*.js

--- js/laolin.app.jg.js
+++ js/laolin.app.jg.js
@@ -1,6 +1,8 @@
 $(function(){laolin.wait.ready(function(){
    
-    laolin.ui.showInfo('欢迎光临老林结构助手-1',8000);
+  //laolin.ui.showInfo('欢迎光临老林结构助手-1',8000);
+  laolin.app.jg={};
   
+  laolin.app.jg.beam_sect={};
   
 })});
