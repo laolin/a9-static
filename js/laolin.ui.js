bd31a8b0424d72de20ea94d5757fef2e2f0ce6e1
LaoLin <hi@laolin.com>
2013-08-11 04:34:03 +0800

    mv laolin.main.js to sever side (inline script)

--- js/laolin.ui.js
+++ js/laolin.ui.js
@@ -15,7 +15,36 @@ $(function(){
     if(undefined==layout)layout='bottomCenter';
     var n = noty({text:text,timeout:timeout,type:type,layout:layout});
     return n;
+  }  
+  /// 利用underscore的template功能，把data数据套用模板tpl，并放在 to 中
+  laolin.ui.template=function(to,tpl,data) {
+    $(to).html( _.template($(tpl).html(),data));
+  };
+  
+  /// ajax调用url返回的RESTful数据
+  /// 利用underscore的template功能，把 数据套用模板tpl，并放在 to 中
+  laolin.ui.templateRest=function(to,tpl,url) {
+    console.log(1);
+    $.getJSON(url,function(data){
+      
+      laolin.ui.template(to,tpl,data);
+    });
+  };
+  
+  
+  /// 取得URL中的?后的参数
+  /// 参考http://stackoverflow.com/questions/901115/how-can-i-get-query-string-values
+  laolin.ui.getParameterByName=function (name) {
+    name = name.replace(/[\[]/, "\\\[").replace(/[\]]/, "\\\]");
+    var regexS = "[\\?&]" + name + "=([^&#]*)";
+    var regex = new RegExp(regexS);
+    var results = regex.exec(window.location.search);
+    if(results == null)
+      return "";
+    else
+      return decodeURIComponent(results[1].replace(/\+/g, " "));
   }
+
   
   /// ========
   laolin.ui.init();
